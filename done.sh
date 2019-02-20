f211a84c4211380bb1badef05ebc28de122ddda2
Peter Andersson <peter@it-slav.net>
2019-02-20 19:58:28 +0100

    Create done.sh

---
+++ done.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "------------------------------------"
+echo "Well done, application is pushed to production"
+echo "See the production app at: https://prod-web-app.cap.it-slav.net/"
+echo "------------------------------------"
