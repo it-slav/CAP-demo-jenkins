e5fafbaf9eba6bc5f4afaec266ab4b22c3fbfe46
Peter Andersson <peter@it-slav.net>
2019-02-20 20:35:07 +0100

    Update done.sh

--- done.sh
+++ done.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "------------------------------------"
 echo "Well done, application is pushed to production"
-echo "See the production app at: https://prod-web-app.cap.it-slav.net/"
+echo "See the production app at: https://prod-jenkins-app.suselinux.info"
 echo "------------------------------------"
